4b3306d5334424f1500140fcd818f05f8360c143
ankitt01 <ankitrawat0102@gmail.com>
2021-09-19 08:30:16 +0530

    solar roof section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,6 +10,7 @@ function Home() {
             <Section title="Model 3" description="Order Online for Touchless Delivery" backgroundImg="model-3.jpg" leftBtnText="Custom order" rightBtnText="Existing inventory" />
             <Section title="Model X" description="Order Online for Touchless Delivery" backgroundImg="model-x.jpg" leftBtnText="Custom order" rightBtnText="Existing inventory" />
             <Section title="Lowest Cost Solar Panels in America" description="Money-back guarantee" backgroundImg="solar-panel.jpg" leftBtnText="Order Now" rightBtnText="Learn More" />
+            <Section title="Solar for New Roofs" description="Solar Roofs Costs Less Than a New Roof Plus Solar Panels" backgroundImg="solar-roof.jpg" leftBtnText="Order Now" rightBtnText="Learn More" />
         </Container>
     )
 }
