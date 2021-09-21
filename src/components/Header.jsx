d5d3e28e052e173239697fd0a35a7b28209b1442
ankitt01 <ankitrawat0102@gmail.com>
2021-09-21 14:48:24 +0530

    Ham burger functions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false);
     return (
         <Container>
             <a>
@@ -13,12 +15,34 @@ function Header() {
                 <a href="#">Model X</a>
                 <a href="#">Model Y</a>
                 <a href="#">Model S</a>
+                <a href="#">Solar Roofs</a>
+                <a href="#">Solar Panels</a>
             </Menu>
             <RightMenu>
                 <a href="#" >Shop</a>
                 <a href="#" >Tesla Account</a>
-                <CustomMenu />
+                <CustomMenu onClick={() => setBurgerStatus(true) } />
             </RightMenu>
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={() => setBurgerStatus(false) } />
+                </CloseWrapper>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-in</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadster</a></li>
+                <li><a href="#">Test Drive</a></li>
+                <li><a href="#">Semi</a></li>
+                <li><a href="#">Charging</a></li>
+                <li><a href="#">Powerwall</a></li>
+                <li><a href="#">Commercial Energy</a></li>
+                <li><a href="#">Utilities</a></li>
+                <li><a href="#">Find Us</a></li>
+                <li><a href="#">Support</a></li>
+                <li><a href="#">Investor Relations</a></li>
+
+            </BurgerNav>
         </Container>
     )
 }
@@ -36,6 +60,7 @@ const Container = styled.div`
     align-items: center;
     justify-content: space-between;
     padding: 0 20px;
+    z-index: 1;
 `
 const Menu = styled.div`
     display: flex;
@@ -66,4 +91,36 @@ const RightMenu = styled.div`
 
 const CustomMenu = styled(MenuIcon)`
     cursor: pointer;
+`
+const BurgerNav = styled.div`
+    position: fixed;
+    top:0;
+    bottom:0;
+    right:0;
+    background-color: white;
+    width: 300px;
+    z-index: 100000;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+    transition: transform 0.2s ease;
+    li {
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0,0,0,.2);
+        a {
+            font-weight: 600;
+        }
+    }
+`
+
+const CustomClose = styled(CloseIcon)`
+cursor: pointer;
+`
+
+const CloseWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
 `
